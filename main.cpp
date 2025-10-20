5f14245b8c421652b203b948f0129f2a31cffd27
Yodua <jakub.jodlowski11@gmail.com>
2025-10-20 23:06:05 +0200

    added someting

--- main.cpp
+++ main.cpp
@@ -1,6 +1,29 @@
+#include <fstream>
 #include <iostream>
+#include <vector>
 
-int main() {
+using namespace std;
+
+vector<vector<string>> graf;
+
+int main(int argc, char* argv[]) {
+
+    // sprawdzanie poprawnosci uruchomienia programu
+    if (argc != 2) {
+        cerr << "Uzycie: " << argv[0] << " <nazwa_pliku>" << endl;
+        return 1;
+    }
+
+    // odczyt z pliku
+    ifstream plik(argv[1]);
+
+    // sprawdzanie czy plik sie otworzyl
+    if (!plik.is_open()) {
+        cerr << "Nie mozna otworzyc pliku" << endl;
+        return 1;
+    }
+
+
 
     return 0;
 }
